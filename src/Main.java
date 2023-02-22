d0be198a03aa7457c328f1f1b2364c6f1e4af113
Копылова Диана Александровна <d.kopylova@s7.ru>
2023-02-22 12:49:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task02.task0204;
+
+/*
+9-ка это перевернутая 6-ка
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+        int six = 6;
+
+
+        six++;
+        six++;
+        six++;
+
+        System.out.println(sikx);
+    }
+}
